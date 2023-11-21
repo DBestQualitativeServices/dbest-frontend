c05da4beffd687668cc7a5b62a6524443fc6f33a
codanMartin <codanmartin_25@gmail.com>
2023-11-21 12:08:38 +0200

    updated dep

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import "./i18n"
+import "./i18n.js"
 
 import {BrowserRouter} from 'react-router-dom';
-import i18next from "i18next";
-import {I18nextProvider} from "react-i18next";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
         <BrowserRouter>
